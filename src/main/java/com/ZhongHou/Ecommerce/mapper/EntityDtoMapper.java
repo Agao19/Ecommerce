427e7dd7b996fdb8565e67c8e77dc0a831a0325e
Trunghau <hauvo555@gmail.com>
2025-01-27 09:57:06 +0700

    m2

--- src/main/java/com/ZhongHou/Ecommerce/mapper/EntityDtoMapper.java
+++ src/main/java/com/ZhongHou/Ecommerce/mapper/EntityDtoMapper.java
@@ -1,9 +1,7 @@
 package com.ZhongHou.Ecommerce.mapper;
 
-import com.ZhongHou.Ecommerce.dto.AddressDto;
-import com.ZhongHou.Ecommerce.dto.UserDto;
-import com.ZhongHou.Ecommerce.entity.Address;
-import com.ZhongHou.Ecommerce.entity.User;
+import com.ZhongHou.Ecommerce.dto.*;
+import com.ZhongHou.Ecommerce.entity.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -26,7 +24,70 @@ public class EntityDtoMapper {
         AddressDto addressDto = new AddressDto();
         addressDto.setId(add.getId());
         addressDto.setCity(add.getCity());
-
+        addressDto.setState(add.getState());
+        addressDto.setStreet(add.getStreet());
+        addressDto.setCountry(add.getCountry());
+        addressDto.setZipCode(add.getZipCode());
         return addressDto;
     }
+
+    //Category to DTO
+    public CategoryDto mapCategoryToDtoBasic(Category category){
+        CategoryDto categoryDto=new CategoryDto();
+        categoryDto.setId(categoryDto.getId());
+        categoryDto.setName(category.getName());
+        return categoryDto;
+    }
+
+    //OrderItem to Dto
+    public OrderItemDto mapOrderItemToDtoBasic(OrderItem orderItem){
+        OrderItemDto orderItemDto = new OrderItemDto();
+        orderItemDto.setId(orderItem.getId());
+        orderItemDto.setQuantity(orderItem.getQuantity());
+        orderItemDto.setPrice(orderItem.getPrice());
+        orderItemDto.setStatus(orderItemDto.getStatus());
+        orderItemDto.setCreatedAt(orderItem.getCreatedAt());
+
+        return  orderItemDto;
+    }
+
+    //Product to DTO
+    public ProductDto mapProductToDtoBasic(Product product){
+        ProductDto productDto = new ProductDto();
+        productDto.setId(product.getId());
+        productDto.setName(product.getName());
+        productDto.setDescription(product.getDescription());
+        productDto.setPrice(product.getPrice());
+        productDto.setImageUrl(product.getImageUrl());
+        return productDto;
+    }
+
+    /////
+    public UserDto mapUserToDtoPlusAddress(User user){
+        UserDto userDto = mapUserToDtoBasic(user);
+        if(user.getAddress() != null) {
+            AddressDto addressDto = mapAddressToDtoBasic(user.getAddress());
+            userDto.setAddress(addressDto);
+        }
+        return userDto;
+    }
+
+    //orderItem to Dto plus product
+    public OrderItemDto mapOrderItemToDtoPlusProduct(OrderItem orderItem){
+        OrderItemDto orderItemDto = mapOrderItemToDtoBasic(orderItem);
+
+        if (orderItemDto.getProduct() != null){
+            ProductDto productDto= mapProductToDtoBasic(orderItem.getProduct());
+            orderItemDto.setProduct(productDto);
+        }
+        return orderItemDto;
+    }
+
+    //OrderItem to Dto plus product adn user
+
+
+
+
+
+
 }
