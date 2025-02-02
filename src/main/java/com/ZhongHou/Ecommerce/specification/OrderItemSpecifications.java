c3e6c07749b394a4ad25569b5a8a9744607c2af1
Trunghau <hauvo555@gmail.com>
2025-02-02 16:45:17 +0700

    status

--- src/main/java/com/ZhongHou/Ecommerce/specification/OrderItemSpecifications.java
+++ src/main/java/com/ZhongHou/Ecommerce/specification/OrderItemSpecifications.java
@@ -1,6 +1,15 @@
 package com.ZhongHou.Ecommerce.specification;
 
+import com.ZhongHou.Ecommerce.entity.OrderItem;
+import com.ZhongHou.Ecommerce.enums.OrderStatus;
+import org.springframework.data.jpa.domain.Specification;
+
 public class OrderItemSpecifications {
 
+    public static Specification<OrderItem> hasStatus(OrderStatus status){
+        return (((root, query, criteriaBuilder) ->
+                status != null ? criteriaBuilder.equal(root.get("status"),status):null));
+
+    }
 
 }
