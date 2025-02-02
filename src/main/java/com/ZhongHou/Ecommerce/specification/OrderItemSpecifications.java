7292559eac3a6d401a8a461f7953c75361579851
Trunghau <hauvo555@gmail.com>
2025-02-02 16:50:37 +0700

    datetime

--- src/main/java/com/ZhongHou/Ecommerce/specification/OrderItemSpecifications.java
+++ src/main/java/com/ZhongHou/Ecommerce/specification/OrderItemSpecifications.java
@@ -4,6 +4,8 @@ import com.ZhongHou.Ecommerce.entity.OrderItem;
 import com.ZhongHou.Ecommerce.enums.OrderStatus;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.time.LocalDateTime;
+
 public class OrderItemSpecifications {
 
     public static Specification<OrderItem> hasStatus(OrderStatus status){
@@ -11,5 +13,21 @@ public class OrderItemSpecifications {
                 status != null ? criteriaBuilder.equal(root.get("status"),status):null));
 
     }
+
+    public static Specification<OrderItem> createdBetween(LocalDateTime startDate, LocalDateTime endDate){
+        return (((root, query, criteriaBuilder) -> {
+            if (startDate != null && endDate != null){
+                return criteriaBuilder.between(root.get("createdAt"),startDate,endDate);
+            } else if (startDate != null){
+                return criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"),startDate);
+            } else if (endDate != null) {
+                return criteriaBuilder.lessThanOrEqualTo(root.get("createdAt"),endDate);
+            }else {
+                return null;
+            }
+        }));
+    }
+
+
 
 }
