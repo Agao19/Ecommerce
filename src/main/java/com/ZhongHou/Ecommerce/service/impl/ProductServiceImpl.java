566a3ba499bd4554dd30deaabcde0f16ee1ba5e1
Trunghau <hauvo555@gmail.com>
2025-02-04 21:26:33 +0700

    createProduct

--- src/main/java/com/ZhongHou/Ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ZhongHou/Ecommerce/service/impl/ProductServiceImpl.java
@@ -1,22 +1,51 @@
 package com.ZhongHou.Ecommerce.service.impl;
 
 import com.ZhongHou.Ecommerce.dto.Response;
+import com.ZhongHou.Ecommerce.entity.Category;
+import com.ZhongHou.Ecommerce.entity.Product;
+import com.ZhongHou.Ecommerce.exception.NotFoundException;
+import com.ZhongHou.Ecommerce.mapper.EntityDtoMapper;
+import com.ZhongHou.Ecommerce.repository.CategoryRepository;
+import com.ZhongHou.Ecommerce.repository.ProductRepository;
 import com.ZhongHou.Ecommerce.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final EntityDtoMapper entityDtoMapper;
 
     @Override
     public Response createProduct(Long categoryId, MultipartFile image, String name, String description, BigDecimal price) {
-        return null;
+        Category category= categoryRepository.findById(categoryId).orElseThrow(()->new NotFoundException("Category not found"));
+        //String productImageUrl=
+
+        Product product = new Product();
+        product.setCategory(category);
+        product.setName(name);
+        product.setDescription(description);
+        //product.setImageUrl(productImageUrl);
+
+        productRepository.save(product);
+
+        return Response.builder()
+                .status(200)
+                .message("Product successfully created")
+                .build();
     }
 
     @Override
     public Response updateProduct(Long productId, Long categoryId, MultipartFile image, String name, String description, BigDecimal price) {
-        return null;
+        Product product=productRepository.findBy()
     }
 
     @Override
