4107b463f9edf007440021d1c8c6ea5f12132175
Trunghau <hauvo555@gmail.com>
2025-02-04 22:21:39 +0700

    place order v1

--- src/main/java/com/ZhongHou/Ecommerce/service/impl/OrderItemServiceImpl.java
+++ src/main/java/com/ZhongHou/Ecommerce/service/impl/OrderItemServiceImpl.java
@@ -2,8 +2,14 @@ package com.ZhongHou.Ecommerce.service.impl;
 
 import com.ZhongHou.Ecommerce.dto.OrderRequest;
 import com.ZhongHou.Ecommerce.dto.Response;
+import com.ZhongHou.Ecommerce.entity.User;
 import com.ZhongHou.Ecommerce.enums.OrderStatus;
+import com.ZhongHou.Ecommerce.mapper.EntityDtoMapper;
+import com.ZhongHou.Ecommerce.repository.OrderItemRepository;
+import com.ZhongHou.Ecommerce.repository.OrderRepository;
+import com.ZhongHou.Ecommerce.repository.ProductRepository;
 import com.ZhongHou.Ecommerce.service.OrderItemService;
+import com.ZhongHou.Ecommerce.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,8 +21,20 @@ import java.time.LocalDateTime;
 @Slf4j
 @RequiredArgsConstructor
 public class OrderItemServiceImpl implements OrderItemService {
+
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final UserService userService;
+    private final EntityDtoMapper entityDtoMapper;
+
     @Override
     public Response placeOrder(OrderRequest orderRequest) {
+
+        User user = userService.getLoginUser();
+
+        //map order request items to order entities
+
         return null;
     }
 
