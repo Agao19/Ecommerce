28266b5b490496cdecb4ddee6a5a86917fc768d1
Trunghau <hauvo555@gmail.com>
2025-06-04 11:38:31 +0700

    register

--- frontend/ecommerce-angular/src/app/register/register.component.ts
+++ frontend/ecommerce-angular/src/app/register/register.component.ts
@@ -1,11 +1,64 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { ApiService } from '../service/api.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  constructor(private router: Router, private readonly apiService: ApiService) {}
+  formData: any={
+    email: '',
+    name: '',
+    phoneNumber: '',
+    password: '',
+  }
 
+  message: any = null;
+
+  async handleSubmit(){
+    if(!this.formData.email || !this.formData.name || !this.formData.phoneNumber || !this.formData.password){
+      this.message = 'Please fill all the fields';
+    }
+    try {
+      const response: any= await firstValueFrom(this.apiService.registerUser(this.formData));
+      if(response.status ===200){
+        this.message = 'Registration successful';
+        this.router.navigate(['/login']);
+      }
+    } catch (error: any) {
+      console.log(error);
+      this.showMessage(error.error?.message || error.message || 'unable to register');
+    }
+  }
+
+  showMessage(message: string){
+    this.message = message;
+    setTimeout(()=>{
+      this.message = null;  
+    }, 3000);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 }
