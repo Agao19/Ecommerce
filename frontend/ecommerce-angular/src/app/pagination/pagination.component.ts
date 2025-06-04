ffcaae43d955bc9c4c9c6d0a582208cfae678032
Trunghau <hauvo555@gmail.com>
2025-06-04 21:32:09 +0700

    pagination

--- frontend/ecommerce-angular/src/app/pagination/pagination.component.ts
+++ frontend/ecommerce-angular/src/app/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './pagination.component.css'
 })
 export class PaginationComponent {
+
+  @Input() currentPage: number=1;
+  @Input() totalPages: number=1;
+  @Input() pageChange = new EventEmitter<number>();
+
+  get pageNumbers(): number[]{
+    return Array.from({length: this.totalPages},(_,i) => i+1)
+  }
+
+  changepage(page: number): void{
+    this.pageChange.emit(page)
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 }
