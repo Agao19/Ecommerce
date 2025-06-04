dd3a2f8819a1238e1b55124a5c5e2d797d3a1e2f
Trunghau <hauvo555@gmail.com>
2025-06-04 11:54:09 +0700

    test reg

--- frontend/ecommerce-angular/src/app/register/register.component.ts
+++ frontend/ecommerce-angular/src/app/register/register.component.ts
@@ -1,64 +1,55 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
 import { ApiService } from '../service/api.service';
+import { Router, RouterLink } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
   imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  constructor(private router: Router, private readonly apiService: ApiService) {}
-  formData: any={
+
+  constructor(private readonly apiService: ApiService, private router: Router) { }
+  formData: any = {
     email: '',
     name: '',
     phoneNumber: '',
-    password: '',
+    password: ''
   }
 
   message: any = null;
 
-  async handleSubmit(){
-    if(!this.formData.email || !this.formData.name || !this.formData.phoneNumber || !this.formData.password){
-      this.message = 'Please fill all the fields';
+
+  async handleSubmit() {
+    if (!this.formData.email || !this.formData.name || !this.formData.phoneNumber || !this.formData.password) {
+      this.showMessage("All fields are required")
+      return;
     }
+
     try {
-      const response: any= await firstValueFrom(this.apiService.registerUser(this.formData));
-      if(response.status ===200){
-        this.message = 'Registration successful';
-        this.router.navigate(['/login']);
+      const response: any = await firstValueFrom(this.apiService.registerUser(this.formData));
+      if (response.status === 200) {
+        this.showMessage('User Successfully registered');
+        this.router.navigate(['/login'])
       }
     } catch (error: any) {
-      console.log(error);
-      this.showMessage(error.error?.message || error.message || 'unable to register');
+        console.log(error)
+        this.showMessage(error.error?.message || error.message || 'unable to register');
     }
   }
 
-  showMessage(message: string){
+
+
+  showMessage(message: string) {
     this.message = message;
-    setTimeout(()=>{
-      this.message = null;  
+    setTimeout(() => {
+      this.message == null
     }, 3000);
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
 }
