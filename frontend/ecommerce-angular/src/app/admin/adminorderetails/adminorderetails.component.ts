9d0fa311c4ea4bbdd525b170d33395c03f79f804
Trunghau <hauvo555@gmail.com>
2025-06-05 21:34:17 +0700

    order details

---
+++ frontend/ecommerce-angular/src/app/admin/adminorderetails/adminorderetails.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminorderetails',
+  imports: [],
+  templateUrl: './adminorderetails.component.html',
+  styleUrl: './adminorderetails.component.css'
+})
+export class AdminorderetailsComponent {
+
+}
