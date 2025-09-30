e5d1a0df1144c685753b91ca0b6ac14791d26bfc
Agao19 <hauvo555@gmail.com>
2025-09-30 19:40:03 +0700

    feat(body): controller

--- backend/src/main/java/com/ZhongHou/Ecommerce/controller/AuthController.java
+++ backend/src/main/java/com/ZhongHou/Ecommerce/controller/AuthController.java
@@ -5,15 +5,14 @@ import com.ZhongHou.Ecommerce.dto.Response;
 import com.ZhongHou.Ecommerce.dto.UserDto;
 import com.ZhongHou.Ecommerce.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
 
     private final UserService userService;
@@ -26,5 +25,13 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<Response> loginUser(@RequestBody LoginRequest loginRequest){
         return ResponseEntity.ok(userService.loginUser(loginRequest));
+    }
+
+    @PostMapping("/logout")
+    void logout(@RequestHeader("Authorization") String authHeader){
+        String token = authHeader.replace("Bearer ", "");
+        log.info("token {}", token);
+        userService.logout(token);
+
     }
 }
