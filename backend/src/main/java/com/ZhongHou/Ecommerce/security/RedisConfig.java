9130f0acbb5aa451c414c15bcd0c4e7129d8e677
Agao19 <hauvo555@gmail.com>
2025-09-30 19:37:06 +0700

    feat(body): use redis

---
+++ backend/src/main/java/com/ZhongHou/Ecommerce/security/RedisConfig.java
@@ -0,0 +1,14 @@
+package com.ZhongHou.Ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public LettuceConnectionFactory RedisConnectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+}
