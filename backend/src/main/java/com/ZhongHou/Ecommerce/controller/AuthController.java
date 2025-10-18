6d598b43ce6a716b584d374968af9094d74bfccd
Agao19 <hauvo555@gmail.com>
2025-10-18 22:52:24 +0700

    feat(authController): change method

--- backend/src/main/java/com/ZhongHou/Ecommerce/controller/AuthController.java
+++ backend/src/main/java/com/ZhongHou/Ecommerce/controller/AuthController.java
@@ -3,9 +3,13 @@ package com.ZhongHou.Ecommerce.controller;
 import com.ZhongHou.Ecommerce.dto.LoginRequest;
 import com.ZhongHou.Ecommerce.dto.Response;
 import com.ZhongHou.Ecommerce.dto.UserDto;
+import com.ZhongHou.Ecommerce.entity.User;
+import com.ZhongHou.Ecommerce.security.AuthUser;
 import com.ZhongHou.Ecommerce.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,9 +27,28 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Response> loginUser(@RequestBody LoginRequest loginRequest){
-        return ResponseEntity.ok(userService.loginUser(loginRequest));
+    public ResponseEntity<Response> loginUser(@RequestBody LoginRequest loginRequest , HttpServletResponse response){
+
+        Response result  = userService.loginUser(loginRequest);
+        // set refresh token vào HttpOnly cookie (Secure=true)
+        ResponseCookie cookie = ResponseCookie.from("rt", result.getRefreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/auth/refresh")
+                .maxAge(result.getExpirationTime())
+                .sameSite("Strict") // hoặc "Lax"/"None" nếu cần cross-site with credentials
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+
+        return ResponseEntity.ok(Response.builder()
+                .status(200)
+                .message("User Successfully Logged In")
+                .token(result.getToken())
+                        .refreshToken(result.getRefreshToken())
+                .role(result.getRole())
+                .build());
     }
+
 
     @PostMapping("/logout")
     void logout(@RequestHeader("Authorization") String authHeader){
@@ -39,7 +62,6 @@ public class AuthController {
     ResponseEntity<Response> newAccessToken(@RequestHeader("Authorization") String authHeader){
         String refreshToken = authHeader.replace("Bearer ", "");
         log.info("new accessToken {}", refreshToken);
-
         return ResponseEntity.ok(userService.sendNewAccessToken(refreshToken));
 
     }
