25b727fcc53529244cba9b8ccd7e06cb19b637a9
Agao19 <hauvo555@gmail.com>
2025-10-21 00:42:11 +0700

    feat(controller): set refresh token with Cookie

--- backend/src/main/java/com/ZhongHou/Ecommerce/controller/AuthController.java
+++ backend/src/main/java/com/ZhongHou/Ecommerce/controller/AuthController.java
@@ -9,6 +9,7 @@ import com.ZhongHou.Ecommerce.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +31,7 @@ public class AuthController {
     public ResponseEntity<Response> loginUser(@RequestBody LoginRequest loginRequest , HttpServletResponse response){
 
         Response result  = userService.loginUser(loginRequest);
+
         // set refresh token vào HttpOnly cookie (Secure=true)
         ResponseCookie cookie = ResponseCookie.from("rt", result.getRefreshToken())
                 .httpOnly(true)
@@ -38,31 +40,54 @@ public class AuthController {
                 .maxAge(result.getExpirationTime())
                 .sameSite("Strict") // hoặc "Lax"/"None" nếu cần cross-site with credentials
                 .build();
-        response.addHeader("Set-Cookie", cookie.toString());
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 
         return ResponseEntity.ok(Response.builder()
                 .status(200)
                 .message("User Successfully Logged In")
                 .token(result.getToken())
-                        .refreshToken(result.getRefreshToken())
+                       // .refreshToken(result.getRefreshToken())
                 .role(result.getRole())
                 .build());
     }
 
+    @PostMapping("/refresh")
+    ResponseEntity<Response> newAccessToken(@CookieValue(name="rt", required = false) String refreshToken,
+                                            HttpServletResponse response){
+        //Check cookie exist?
+        if (refreshToken == null || refreshToken.isBlank())
+            return ResponseEntity.status(401).body(
+                    Response.builder().
+                            status(401).message("Refresh token is missing.").
+                            build());
+
+
+        Response result = userService.sendNewAccessToken(refreshToken);
+        //Nếu có xoay vòng (newRefreshToken được trả về), set lại cookie MỚI
+        if (result.getRefreshToken() != null) {
+            ResponseCookie newCookie = ResponseCookie.from("rt", result.getRefreshToken())
+                    .httpOnly(true)
+                    .secure(true)
+                    .path("/auth/refresh")
+                    .maxAge(result.getExpirationTime())
+                    .sameSite("Strict")
+                    .build();
+            response.addHeader(HttpHeaders.SET_COOKIE, newCookie.toString());
+        }
+        log.info("role" + result.getRole());
+        return ResponseEntity.ok(Response.builder()
+                .status(200)
+                .message("Successfully with new access token")
+                .token(result.getToken()) // AccessToken MỚI
+                .build());
+
+    }
 
     @PostMapping("/logout")
     void logout(@RequestHeader("Authorization") String authHeader){
         String token = authHeader.replace("Bearer ", "");
         log.info("token {}", token);
         userService.logout(token);
-
-    }
-
-    @PostMapping("/refresh")
-    ResponseEntity<Response> newAccessToken(@RequestHeader("Authorization") String authHeader){
-        String refreshToken = authHeader.replace("Bearer ", "");
-        log.info("new accessToken {}", refreshToken);
-        return ResponseEntity.ok(userService.sendNewAccessToken(refreshToken));
 
     }
 }
