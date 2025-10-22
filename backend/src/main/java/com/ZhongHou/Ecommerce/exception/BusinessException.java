f8e6407f006226ad5cfebf6d078b821b9a674b02
Agao19 <hauvo555@gmail.com>
2025-10-22 08:14:10 +0700

    feat(payment): vnpay config

---
+++ backend/src/main/java/com/ZhongHou/Ecommerce/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.ZhongHou.Ecommerce.exception;
+
+
+public class BusinessException extends RuntimeException {
+
+
+    public BusinessException(String message) {
+        super(message);
+
+    }
+}
