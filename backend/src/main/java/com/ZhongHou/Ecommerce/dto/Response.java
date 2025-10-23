3381e81d38763c5b1c426b46a432df53ff8c32f3
Agao19 <hauvo555@gmail.com>
2025-10-23 12:25:34 +0700

    feat(caching): setting caching (redis + spring cache)

--- backend/src/main/java/com/ZhongHou/Ecommerce/dto/Response.java
+++ backend/src/main/java/com/ZhongHou/Ecommerce/dto/Response.java
@@ -1,19 +1,25 @@
 package com.ZhongHou.Ecommerce.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
 
     private int status;
     private String message;
+    //@JsonIgnore //testing cache
     private final LocalDateTime timestamp=LocalDateTime.now();
 
     private String token;
