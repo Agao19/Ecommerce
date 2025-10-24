ccee0da232f419523a1a32b2da33114305333a18
Agao19 <hauvo555@gmail.com>
2025-10-24 23:15:44 +0700

    feat(vnpay): phải @EnablesSecheduling, @Asyc để test

--- backend/src/main/java/com/ZhongHou/Ecommerce/EcommerceApplication.java
+++ backend/src/main/java/com/ZhongHou/Ecommerce/EcommerceApplication.java
@@ -3,10 +3,14 @@ package com.ZhongHou.Ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 //@EnableCaching
+//@EnableScheduling
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
